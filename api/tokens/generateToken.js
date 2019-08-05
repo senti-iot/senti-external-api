a718d39f22ea5bf85c7ebe92eb65d53409e7863b
Andrei Tudor <andrei.tudor27@gmail.com>
2019-08-05 12:18:09 +0200

    Token verification

--- api/tokens/generateToken.js
+++ api/tokens/generateToken.js
@@ -6,17 +6,59 @@ const moment = require('moment')
 var crypto = require("crypto");
 const mysqlConn = require('../mysqlConn/mysqlconn')
 
-router.get('/validateToken/:id/:typeID', async (req, res) => {
-	let id = req.params.id
+router.get('/validateToken/:rawToken/:type/:typeID', async (req, res) => {
+	let rawToken = req.params.rawToken
 	let typeID = req.params.typeID
+	let type = req.params.type
+	console.log(type)
 	let selectTokenQ = `SELECT * from externalAPI where token=? and type_id=?`
-	let token = sha2['SHA-256'](process.env.vader + id).toString('hex')
-	await mysqlConn.query(selectTokenQ, [token, typeID]).then(rs => {
-		if(rs[0].length > 0){
+	let token = sha2['SHA-256'](process.env.vader + rawToken).toString('hex')
+	console.log(rawToken)
+	console.log(token)
+	await mysqlConn.query(selectTokenQ, [token, typeID]).then(async rs => {
+		if (rs[0].length > 0) {
+			console.log('Valid Token')
 			res.status(200).json(true)
 		}
 		else {
-			res.status(404).json(false)
+			if (type === 'device') {
+				selectTokenQ = `SELECT * from externalAPI where token=? and type=1`
+				await mysqlConn.query(selectTokenQ, [token, typeID]).then(async rs => {
+					if (rs[0].length > 0) {
+						console.log('Vaild Registry token')
+						let checkReg = `SELECT * from Device where id=? and reg_id=?`
+						await mysqlConn.query(checkReg, [typeID, rs[0][0].type_id]).then(rs => {
+							if (rs[0].length > 0) {
+								console.log('Valid Registry/Device token')
+								return res.status(200).json(true)
+							}
+							else {
+								return res.status(404).json(false)
+							}
+						})
+					}
+				})
+				selectTokenQ = `SELECT * from externalAPI where token=? and type=2`
+				await mysqlConn.query(selectTokenQ, [token, typeID]).then(async rs => {
+					if (rs[0].length > 0) {
+						console.log('Vaild Device Type token')
+						let checkReg = `SELECT * from Device where id=? and type_id=?`
+						await mysqlConn.query(checkReg, [typeID, rs[0][0].type_id]).then(rs => {
+							if (rs[0].length > 0) {
+								console.log('Valid DeviceType/Device token')
+								return res.status(200).json(true)
+							}
+							else {
+								return res.status(404).json(false)
+							}
+						})
+					}
+				})
+			}
+
+			else {
+				res.status(404).json(false)
+			}
 		}
 	})
 });
@@ -28,14 +70,13 @@ router.post('/generateToken', async (req, res) => {
 	let name = req.body.name
 	let id = crypto.randomBytes(20).toString('hex').toUpperCase()//?
 	let token = sha2['SHA-256'](process.env.vader + id).toString('hex')
-	console.log('Bing')
 	let storeTokenQ = `INSERT INTO externalAPI
 	(name, token, \`type\`, type_id, created, createdBy)
 	VALUES(?, ?, ?, ?, NOW(), ?);
 	`
 	await mysqlConn.query(storeTokenQ, [name, token, type, typeId, userId]).then(rs => {
-		if(rs[0].insertId > 0) {
-			 res.json(id).status(200);
+		if (rs[0].insertId > 0) {
+			res.json(id).status(200);
 		}
 		else {
 			res.json(false).status(500)
@@ -45,4 +86,4 @@ router.post('/generateToken', async (req, res) => {
 	})
 });
 
-module.exports=router
+module.exports = router
