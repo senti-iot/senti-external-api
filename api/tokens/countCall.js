96b4b1df9073acaebf237f872cf2f6e3c8961a2f
Andrei Tudor <andrei.tudor27@gmail.com>
2021-10-18 18:16:38 +0200

    Fix countCall

--- api/tokens/countCall.js
+++ api/tokens/countCall.js
@@ -9,15 +9,19 @@ router.get('/count/:token', async (req, res) => {
 	let rawToken = req.params.token
 	let selectTokenSQL = `SELECT * from externalAPI where token=?`
 	let token = sha2['SHA-256'](process.env.vader + rawToken).toString('hex')
-	let sToken = mysqlConn.query(selectTokenSQL,[token]).then(rs => rs[0][0])
+	let sToken = await mysqlConn.query(selectTokenSQL, [token]).then(rs => rs[0][0])
+	console.log(sToken)
 	if (sToken) {
 		let countSQL = `
 			UPDATE externalAPI
-			SET count = ${sToken.count + 1}
-			lastCall = ${moment().format('YYYY-MM-DD HH:mm:ss')}
-			WHERE id=?
+			SET count = ${sToken.count + 1},
+			lastCall = "${moment().format('YYYY-MM-DD HH:mm:ss')}"
+			WHERE id=?;
 		`
-		let countQuery = mysqlConn.query(count, [sToken.id])
+		let format = mysqlConn.format(countSQL, [sToken.id])
+		console.log(format)
+		let countQuery = mysqlConn.query(countSQL, [sToken.id])
+
 		return res.status(200).json(countQuery)
 	}
 	else
